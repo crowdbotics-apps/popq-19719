832313d9eb091042feb13962c0ea3f7530e4f26a
Crowdbotics <team@crowdbotics.com>
2020-08-21 23:25:13 +0000

    Update models

--- backend/popq_19719/wsgi.py
+++ backend/popq_19719/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'popq_19719.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "popq_19719.settings")
 
 application = get_wsgi_application()
