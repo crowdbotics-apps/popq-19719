398be8c7678a1bbbb73eb4098e5e910dabebdb57
Crowdbotics <team@crowdbotics.com>
2020-08-21 23:24:32 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/popq_19719/MainActivity.java
@@ -0,0 +1,15 @@
+package com.popq_19719;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "popq_19719";
+    }
+}
